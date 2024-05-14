3eda7815d84c263f7c6c344c402271757f4f986a
joaovictorvasco <168442928+joaovictorvasco@users.noreply.github.com>
2024-05-13 23:21:39 -0300

    Update teste3.py

--- teste3.py
+++ teste3.py
@@ -1,60 +1,30 @@
 import streamlit as st
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
-import re
-from wordcloud import WordCloud, STOPWORDS
-import matplotlib.pyplot as plt
-import numpy as np
 
-DATE_COLUMN = 'date/time'
-DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
-         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+def calculator():
+    st.title("Calculadora Simples")
+    
+    num1 = st.number_input("Digite o primeiro número:")
+    num2 = st.number_input("Digite o segundo número:")
+    
+    operation = st.selectbox("Selecione a operação", ["Soma", "Subtração", "Multiplicação", "Divisão"])
+    
+    if st.button("Calcular"):
+        if operation == "Soma":
+            result = num1 + num2
+            st.success(f"A soma de {num1} e {num2} é {result}.")
+        elif operation == "Subtração":
+            result = num1 - num2
+            st.success(f"A subtração de {num1} e {num2} é {result}.")
+        elif operation == "Multiplicação":
+            result = num1 * num2
+            st.success(f"A multiplicação de {num1} e {num2} é {result}.")
+        elif operation == "Divisão":
+            if num2 == 0:
+                st.error("Impossível dividir por zero.")
+            else:
+                result = num1 / num2
+                st.success(f"A divisão de {num1} por {num2} é {result}.")
 
-@st.cache_data
-def load_data(nrows):
-    data = pd.read_csv(DATA_URL, nrows=nrows)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-  
-def test_file():
-    # Create a text element and let the reader know the data is loading.
-    data_load_state = st.text('Loading data...')
-    # Load 10,000 rows of data into the dataframe.
-    data = load_data(10000)
-    # Notify the reader that the data was successfully loaded.
-    data_load_state.text("Done!")
+if __name__ == "__main__":
+    calculator()
 
-st.title('Teste ECMI')
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.sidebar.header("Select Link")
-links = ["https://seaportai.com/blog-predictive-maintenance/",
-         "https://seaportai.com/healthcare-analytics/",
-         "https://seaportai.com/blog-rpameetsai/",
-         "https://seaportai.com/covid-19/"]
-
-URL = st.sidebar.selectbox('Link', links)
-st.sidebar.header("Select No. of words you want to display")
-words = st.sidebar.selectbox("No. of words", range(10, 1000, 10))
-if URL is not None:
-    r = requests.get(URL)
-    #using the web scraping library that is Beautiful Soup
-    soup = BeautifulSoup(r.content, 'html.parser')
-    #extracting the data that is in 'div' content of HTML page
-    table = soup.find('div', attrs = {'id':'main-content'})
-    text = table.text
-    #cleaning the data with regular expression library
-    cleaned_text = re.sub('\t', "", text)
-    cleaned_texts = re.split('\n', cleaned_text)
-    cleaned_textss = "".join(cleaned_texts)
-    st.write("Word Cloud Plot")
-    #using stopwords to remove extra words
-    stopwords = set(STOPWORDS)
-    wordcloud = WordCloud(background_color = "white", max_words =
-              words,stopwords = stopwords).generate(cleaned_textss)
-    plt.imshow(wordcloud, interpolation = 'bilinear')
-    plt.axis("off")
-    plt.show()
-    st.pyplot()
