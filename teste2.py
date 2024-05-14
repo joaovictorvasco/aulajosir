3ed4e9e6f2c3652cbd38d898c4c4c0cd38384f02
joaovictorvasco <168442928+joaovictorvasco@users.noreply.github.com>
2024-05-13 23:16:21 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -1,9 +1,14 @@
-nome = input("Digite seu nome completo: ")
-if nome.startswith("Joao") or nome.startswith("Maria"):
-  print("Seu nome é bonito!")
+velocidade = int(input("Digite sua velocidade: "))
+limite = 80
+multa_por_5km = 100
+if velocidade > limite:
+  velocidade_acima = velocidade - limite
+  if velocidade_acima <= 4:
+    print(f"Você excedeu o limite de velocidade em {velocidade_acima} km/h!")
+    print(f"Sua multa é de R${multa_por_5km:.2f}.")
+  else:
+    multa = (velocidade_acima // 5) * multa_por_5km
+    print(f"Você excedeu o limite de velocidade em {velocidade_acima} km/h!")
+    print(f"Sua multa é de R${multa:.2f}.")
 else:
-  print("Seu nome é feio!")
-if nome.endswith("Penido"):
-  print("Você é da Família!")
-else:
-  print("Não somos parentes!")
+  print("Parabéns, você está dentro do limite!")
