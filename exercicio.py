23d774a037812ed17b32f991a3a7bef41691e9c0
joaovictorvasco <168442928+joaovictorvasco@users.noreply.github.com>
2024-05-19 18:53:53 -0300

    Update exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -5,34 +5,20 @@ import matplotlib.pyplot as plt
 
 # Função para carregar e processar os dados
 def load_data():
-    try:
-        df = pd.read_csv('drivers.csv')
-        if df.empty:
-            st.error("O arquivo CSV está vazio.")
-            return None
-        grouped_df = df.groupby('Nationality')['PTS'].sum().reset_index()
-        grouped_df = grouped_df.sort_values(by='PTS', ascending=False)
-        return grouped_df
-    except FileNotFoundError:
-        st.error("Arquivo 'drivers.csv' não encontrado.")
-        return None
-    except pd.errors.EmptyDataError:
-        st.error("O arquivo 'drivers.csv' está vazio.")
-        return None
-    except pd.errors.ParserError:
-        st.error("Erro ao analisar o arquivo 'drivers.csv'. Verifique se o arquivo está no formato correto.")
-        return None
+    df = pd.read_csv('drivers.csv')
+    grouped_df = df.groupby('Nationality')['PTS'].sum().reset_index()
+    grouped_df = grouped_df.sort_values(by='PTS', ascending=False)
+    return grouped_df
 
 # Função para criar o gráfico
 def create_bar_chart(data):
-    if data is not None:
-        sns.set(style="whitegrid")
-        plt.figure(figsize=(10, 6))
-        sns.barplot(x='PTS', y='Nationality', data=data, palette='viridis')
-        plt.title('Total de Pontos por Nacionalidade')
-        plt.xlabel('Pontos')
-        plt.ylabel('Nacionalidade')
-        st.pyplot(plt)
+    sns.set(style="whitegrid")
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x='PTS', y='Nationality', data=data, palette='viridis')
+    plt.title('Total de Pontos por Nacionalidade')
+    plt.xlabel('Pontos')
+    plt.ylabel('Nacionalidade')
+    st.pyplot(plt)
 
 # Carregar os dados
 data = load_data()
@@ -42,3 +28,4 @@ st.title('Análise de Pontos por Nacionalidade')
 
 # Mostrar o gráfico
 create_bar_chart(data)
+
